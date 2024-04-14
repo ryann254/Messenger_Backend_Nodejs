0c736e960ca6312f85ae003f6a0f194865e4c9eb
ryann254 <keddelyronjoz@gmail.com>
2024-04-14 11:12:23 +0300

    feat: add mongodb adapter and connect it to a live mongodb cluster

--- index.ts
+++ index.ts
@@ -1,33 +1,8 @@
 import express from 'express';
-import { Server } from 'socket.io';
-import { createServer } from 'http';
+import main from './socket';
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+main().catch((err) => console.error(err));
 
-io.on('connection', (socket) => {
-  // Broadcast that a user is online
-  console.log('a user connected');
-  // Update the current user's online status
-
-  // Get chatrooms associated with the current user(rooms with messages)
-  // Catch all listener for debugging
-  // Handle the message
-  socket.on('message', (msg) => {
-    console.log(msg);
-    socket.broadcast.emit('message', msg);
-  });
-
-  // Handle disconnection
-  // Update the current user's online status
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+export default app;
